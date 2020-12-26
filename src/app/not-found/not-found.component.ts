2e2f5b1c4fdf9464943256da877c05d45bb26c89
Yukichan01 <75578698+Yukichan01@users.noreply.github.com>
2020-12-26 17:41:03 +0800

    Add files via upload

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactListService } from '../contact-list/contact-list.service';
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  ngOnInit(){
+  
+}
+
+}
