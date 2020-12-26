9bfddc3e2fa1ec33b960fafabbc812a8a0c4ad77
Yukichan01 <75578698+Yukichan01@users.noreply.github.com>
2020-12-26 17:39:23 +0800

    Add files via upload

---
+++ src/shared/contact.model.ts
@@ -0,0 +1,13 @@
+export class Contact{
+    public id: number;
+    public name: string;
+    public email: string;
+    public phone: string;
+    
+    constructor(id: number,name: string,email: string,phone: string){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
